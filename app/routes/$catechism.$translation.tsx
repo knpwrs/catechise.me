9ac81f70fa4c038a605e9451957e9294b8c67e09
Ken Powers <ken@kenpowers.net>
2022-02-26 22:54:02 -0600

    Implement basic navigation

--- app/routes/$catechism.$translation.tsx
+++ app/routes/$catechism.$translation.tsx
@@ -1,5 +1,11 @@
-import { AppShell, Header, Title } from '@mantine/core';
-import { LinksFunction, LoaderFunction, useLoaderData } from 'remix';
+import { AppShell, Header, Select, SimpleGrid, Title } from '@mantine/core';
+import { useCallback } from 'react';
+import {
+  LinksFunction,
+  LoaderFunction,
+  useLoaderData,
+  useNavigate,
+} from 'remix';
 import sanitize from 'sanitize.css';
 
 export const links: LinksFunction = () => [
@@ -24,14 +30,51 @@ export const loader: LoaderFunction = ({ params }): LoaderData => {
 
 export default () => {
   const data = useLoaderData<LoaderData>();
+  const navigate = useNavigate();
+
+  const onChangeCatechism = useCallback(
+    (value: string) => {
+      navigate(`/${value}/${data.translation}`);
+    },
+    [data.translation, navigate],
+  );
+
+  const onChangeTranslation = useCallback(
+    (value: string) => {
+      navigate(`/${data.catechism}/${value}`);
+    },
+    [data.catechism, navigate],
+  );
 
   return (
     <AppShell
       fixed
       padding="md"
       header={
-        <Header height={60} padding="xs">
+        <Header
+          height={60}
+          padding="xs"
+          sx={{ display: 'flex', justifyContent: 'space-between' }}
+        >
           <Title order={1}>catechise.me</Title>
+          <SimpleGrid cols={2}>
+            <Select
+              value={data.catechism}
+              data={[
+                { label: "Keach's Catechism", value: 'keach' },
+                { label: "Spugeon's Catechism", value: 'spurgeon' },
+              ]}
+              onChange={onChangeCatechism}
+            />
+            <Select
+              value={data.translation}
+              data={[
+                { label: 'ESV', value: 'esv' },
+                { label: 'NASB', value: 'nasb' },
+              ]}
+              onChange={onChangeTranslation}
+            />
+          </SimpleGrid>
         </Header>
       }
     >
